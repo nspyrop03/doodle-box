348883c447fc5c646a35adedf85c423450a4a999
Nikolas Spyropoulos <nick.spyrop03@gmail.com>
2026-03-21 13:48:15 +0200

    Changed to WiFiManager

--- doodle_box_main.ino
+++ doodle_box_main.ino
@@ -3,14 +3,13 @@
 #include <PubSubClient.h>
 #include <GxEPD2_BW.h>
 #include <driver/rtc_io.h>
+#include <WiFiManager.h>
 
 #include "secrets.h"
 
 #define WAKE_BUTTON GPIO_NUM_33
+#define BUZZER_PIN 21
 #define IMAGE_BYTES 4736
-
-const char* ssid = SECRET_WIFI_SSID;
-const char* password = SECRET_WIFI_PASS;
 
 const char* mqtt_server = SECRET_MQTT_URL;
 const char* mqtt_user = SECRET_MQTT_USER;
@@ -29,6 +28,7 @@ PubSubClient client(espClient);
 bool newDrawingReceived = false;
 RTC_DATA_ATTR byte lastDrawing[IMAGE_BYTES];
 RTC_DATA_ATTR bool isFirstBoot = true;
+bool wokeUpFromButton = false;
 
 bool isDrawingBlank(byte* payload, unsigned int length) {
   byte firstByte = payload[0];
@@ -78,29 +78,59 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
         // We pass BOTH colors: GxEPD_WHITE for '1' bits, and GxEPD_BLACK for '0' bits
         display.drawBitmap(0, 0, payload, 296, 128, GxEPD_WHITE, GxEPD_BLACK);
       } while (display.nextPage());
+
+      if (wokeUpFromButton) {
+        Serial.println("Beeping buzzer...");
+        digitalWrite(BUZZER_PIN, LOW);  delay(150); 
+        digitalWrite(BUZZER_PIN, HIGH); delay(100); 
+        digitalWrite(BUZZER_PIN, LOW);  delay(150); 
+        digitalWrite(BUZZER_PIN, HIGH);
+      } else {
+        Serial.println("Timer Wakeup: Screen updated silently!");
+      }
     }
+
     newDrawingReceived = true;
   }
 }
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, HIGH);
+  
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0) {
+    Serial.println("Woke up from physical Button Press!");
+    wokeUpFromButton = true;
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
+    Serial.println("Woke up automatically from Timer!");
+    wokeUpFromButton = false;
+  } else {
+    Serial.println("First boot or reset.");
+    // Beep on the very first time you power it on, just to check that the speaker is okay
+    wokeUpFromButton = true; 
+  }
+
   Serial.println("\n\nWAKING UP\n\n");
   
   // Initialize E-ink display
   display.init(115200, true, 2, false);
   display.setRotation(1);
 
-  // Connect to Wi-Fi
-  Serial.print("Connecting to Wi-Fi");
-  WiFi.begin(ssid, password);
+  Serial.println("Trying to connect to the WiFi through the WiFiManager...");
+  //WiFi.mode(WIFI_STA);
+  WiFi.setTxPower(WIFI_POWER_8_5dBm); // low power
 
-  int wifiAttempts = 0;
-  while (WiFi.status() != WL_CONNECTED && wifiAttempts < 20) {
-    delay(500);
-    Serial.print(".");
-    wifiAttempts++;
-  }
+  String macText = String(Network.macAddress());
+  Serial.println("MAC Address: " + macText);
+
+  WiFiManager wifiManager;
+  String setupName = "DoodleBox-Setup-" + macText;
+  wifiManager.setConfigPortalTimeout(5 * 60); // keep the portal alive for 5 minutes
+  wifiManager.autoConnect(setupName.c_str());
 
   if(WiFi.status() == WL_CONNECTED) {
     Serial.println("\nWi-Fi Connected!");
@@ -111,15 +141,20 @@ void setup() {
     client.setCallback(mqttCallback);
     client.setBufferSize(6000); // Crucial for image data size
 
-    if(client.connect("DoodleBox_Device", mqtt_user, mqtt_pass)) {
+    String clientId = "DoodleBox-"; // apparently Arduino has a very convenient but slow String class 
+    clientId += macText;
+
+    if(client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
       Serial.println("Connected to HiveMQ Cloud!");
       client.subscribe("project/drawing");
 
       Serial.println("Checking mailbox...");
-      unsigned long startTime = millis();
-      while(millis() - startTime < 5000) {
+      int mailAttempts = 0;
+      const int MAX_MAIL_ATTEMPTS = 20;
+      while(mailAttempts < MAX_MAIL_ATTEMPTS && !newDrawingReceived) {
         client.loop();
-        delay(10);
+        delay(100);
+        mailAttempts++;
       }
       
       if(!newDrawingReceived) {
@@ -127,7 +162,7 @@ void setup() {
       }
     } else {
       Serial.print("Failed, rc=");
-      Serial.print(client.state());
+      Serial.println(client.state());
     }
   } else {
     Serial.println("Failed to connect to the WiFi!");
@@ -138,14 +173,13 @@ void setup() {
   // Cut power to the screen to save battery and prevent burn-in
   display.hibernate(); 
 
-  // Configure the Wake Button
-  // Tell it to wake up when Pin 33 goes LOW (0)
+  // Configure the Wake Button: wake up when Pin 33 goes LOW (0)
   esp_sleep_enable_ext0_wakeup(WAKE_BUTTON, 0); 
   // Turn on the internal pull-up resistor
   rtc_gpio_pullup_en(WAKE_BUTTON);
   rtc_gpio_pulldown_dis(WAKE_BUTTON);
 
-  uint64_t time_in_sec = 60;
+  const uint64_t time_in_sec = 60;
   esp_sleep_enable_timer_wakeup(time_in_sec * 1000000ULL); // convert to ms
 
   esp_deep_sleep_start();
